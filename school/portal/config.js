50fb96fe32092e960db6e45b75560cc5a195c861
pentech225 <pentech225@gmail.com>
2026-07-24 15:07:33 +0900

    デプロイ

--- school/portal/config.js
+++ school/portal/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     // 個人アカウント（登録・ログイン・進捗管理）のバックエンドURL
     // school/portal/portal-gas-code.js をGoogle Apps Scriptにデプロイして、
     // 発行されたウェブアプリURLをここに設定してください。
-    PORTAL_GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyIIq7pYWuGQnZsXo6WrMR5yDNjIkcXkBlgLv8ZF80giSR8FzUcUdeyXrRZbmkyy0PUWg/exec',
+    PORTAL_GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby-RWFB3bpV4ayQDcgF1N6Rghpa1_vERo6iaTnIObtpluwpg1J7-znHahnywh-Z-cqaaA/exec',
 
     // ITパスポートコースの先生用管理画面（宿題の割り当て）のパスワード
     TEACHER_PASSWORD: 'teacher26'
