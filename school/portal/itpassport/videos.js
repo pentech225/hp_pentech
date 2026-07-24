b5cbe8253e415c33dac578a9a7d69ed704dc2f84
pentech225 <pentech225@gmail.com>
2026-07-24 15:29:41 +0900

    動画タイトルに数字を追加

--- school/portal/itpassport/videos.js
+++ school/portal/itpassport/videos.js
@@ -12,19 +12,19 @@
  */
 
 const VIDEOS = [
-  { videoId: 'hsNKCU20vBU', title: '【ITパスポート】受験メリット・取得方法／ITの国家試験とは？', order: 1 },
-  { videoId: 'inAhVU7-LXY', title: 'ITパスポート試験 出題範囲、テスト形式、IT国家試験の取得メリット', order: 2 },
-  { videoId: 'lZsR_8Eugi4', title: 'ITすきま教室の使い方【ITパスポート,基本情報技術者,高校情報科】', order: 3 },
-  { videoId: 'UxBUwPuepdI', title: 'ITパスポート試験 テクノロジ系分野の学習内容ダイジェスト', order: 4 },
-  { videoId: '4TSLy0SyxtI', title: 'IPアドレスとは？サーバーとクライアント／ITパスポート・基本情報技術者・高校情報', order: 5 },
-  { videoId: 'ciZm4KDwS7M', title: 'コンピューターの五大装置(ハードウェア)／ITパスポート・基本情報技術者試験・高校情報Ⅰ', order: 6 },
-  { videoId: 'ZkArQb3mIQc', title: 'CPUのパーツの役割（コア、プロセッサ、クロック周波数、GPUとCPU、レジスタ）', order: 7 },
-  { videoId: 'Dz3GkHczSQM', title: 'コンピューターの記憶装置（主記憶装置と補助記憶装置の違い、キャッシュメモリ、HDD、SSD）', order: 8 },
-  { videoId: 'S-Xzi7oGxhQ', title: 'RAIDとは？ハードディスクの多重化・ハードウェア障害からの保護', order: 9 },
-  { videoId: 'DJfC3iOmRZk', title: 'ソフトウェアを知ろう オープンソースソフトウェアの特徴・改良と再配布', order: 10 },
-  { videoId: 'GfBmj9T09YQ', title: 'ITパスポート／出力装置・試験に向けた知識対策！（CAD/CAM、DPIなど）', order: 11 },
-  { videoId: '-bF3t5oaRTk', title: 'ネットワーク通信とは【IPアドレス/サーバー/通信プロトコル】', order: 12 },
-  { videoId: 'uBcTnwSGqDs', title: 'ドメインルール・IPアドレス枯渇化対策／ITパスポート・基本情報技術者・高校情報', order: 13 },
-  { videoId: 'DV-zl0OnWto', title: 'ファイルディレクトリのパス指定問題・CUIとGUI', order: 14 },
-  { videoId: 'nfTL64JxU-w', title: 'キャッシュ、クッキーの違いを解説！セッションとは？', order: 15 }
+  { videoId: 'hsNKCU20vBU', title: '①【ITパスポート】受験メリット・取得方法／ITの国家試験とは？', order: 1 },
+  { videoId: 'inAhVU7-LXY', title: '②ITパスポート試験 出題範囲、テスト形式、IT国家試験の取得メリット', order: 2 },
+  { videoId: 'lZsR_8Eugi4', title: '③ITすきま教室の使い方【ITパスポート,基本情報技術者,高校情報科】', order: 3 },
+  { videoId: 'UxBUwPuepdI', title: '④ITパスポート試験 テクノロジ系分野の学習内容ダイジェスト', order: 4 },
+  { videoId: '4TSLy0SyxtI', title: '⑤IPアドレスとは？サーバーとクライアント／ITパスポート・基本情報技術者・高校情報', order: 5 },
+  { videoId: 'ciZm4KDwS7M', title: '⑥コンピューターの五大装置(ハードウェア)／ITパスポート・基本情報技術者試験・高校情報Ⅰ', order: 6 },
+  { videoId: 'ZkArQb3mIQc', title: '⑦CPUのパーツの役割（コア、プロセッサ、クロック周波数、GPUとCPU、レジスタ）', order: 7 },
+  { videoId: 'Dz3GkHczSQM', title: '⑧コンピューターの記憶装置（主記憶装置と補助記憶装置の違い、キャッシュメモリ、HDD、SSD）', order: 8 },
+  { videoId: 'S-Xzi7oGxhQ', title: '⑨RAIDとは？ハードディスクの多重化・ハードウェア障害からの保護', order: 9 },
+  { videoId: 'DJfC3iOmRZk', title: '⑩ソフトウェアを知ろう オープンソースソフトウェアの特徴・改良と再配布', order: 10 },
+  { videoId: 'GfBmj9T09YQ', title: '⑪ITパスポート／出力装置・試験に向けた知識対策！（CAD/CAM、DPIなど）', order: 11 },
+  { videoId: '-bF3t5oaRTk', title: '⑫ネットワーク通信とは【IPアドレス/サーバー/通信プロトコル】', order: 12 },
+  { videoId: 'uBcTnwSGqDs', title: '⑬ドメインルール・IPアドレス枯渇化対策／ITパスポート・基本情報技術者・高校情報', order: 13 },
+  { videoId: 'DV-zl0OnWto', title: '⑭ファイルディレクトリのパス指定問題・CUIとGUI', order: 14 },
+  { videoId: 'nfTL64JxU-w', title: '⑮キャッシュ、クッキーの違いを解説！セッションとは？', order: 15 }
 ];
